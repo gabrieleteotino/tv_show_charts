e030410b822de3f580be2cf54277a3029536373a
Gabriele Teotino <ibt.org@gmail.com>
2014-12-31 22:32:45 +0100

    Added a colormap and a different color for each season

--- tv_show_charts/plot_show.py
+++ tv_show_charts/plot_show.py
@@ -14,14 +14,26 @@ class PlotShow(object):
     '''
     def plot_show_multi(self, show, save_file):
         """
-        Each season color-coded 
-        best-fit linear regression line to show the season's trend
+        A chart with each season color-coded
+        Plot "Ratings", "Number of votes" and a best-fit linear regression line to show the season's trend
         """
+
+        print "plot_show_multi - " + str(show)
 
         plt.style.use("ggplot")
 
-        print "plot_show_multi - " + str(show)
-        
+        # These are the "Tableau 20" colors as RGB
+        tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),
+             (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),
+             (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),
+             (227, 119, 194), (247, 182, 210), (127, 127, 127), (199, 199, 199),
+             (188, 189, 34), (219, 219, 141), (23, 190, 207), (158, 218, 229)]
+
+        # Scale the RGB values to the [0, 1] range, which is the format matplotlib accepts
+        for i in range(len(tableau20)):
+            r, g, b = tableau20[i]
+            tableau20[i] = (r / 255., g / 255., b / 255.)
+
         title = "IMDb ratings for " + show.name + " (" + str(show.year) + ")"
         seasons = show.get_seasons()
         
@@ -51,13 +63,14 @@ class PlotShow(object):
             ratings = [ep.rating for ep in season.episodes]
             ratings_trend = calculate_trend_line_poly(x, ratings)
             votes = [ep.votes for ep in season.episodes]
-            votes_trend = calculate_trend_line_poly(x, votes)
+            #votes_trend = calculate_trend_line_poly(x, votes)
             
             # Plot ratings
             axis = axes[i]
-            axis.plot(x, ratings, "-8")
+            color = tableau20[i%20]
+            axis.plot(x, ratings, "-8", color=color)
             # Plot the trend line
-            axis.plot(x, ratings_trend(x), "-")
+            axis.plot(x, ratings_trend(x), "-", color=color)
             axis.set_title("Season {}".format(season.number), fontsize=12)
             # Configure the axis
             axis.set_ylim(5, 10)
@@ -68,10 +81,10 @@ class PlotShow(object):
             axes_twin.append(axis_twin)
             
             # Plot votes
-            axis_twin.plot(x, votes, "--")
+            axis_twin.plot(x, votes, "--", color=color)
             axis_twin.set_ylim(0, max_votes)
             # Plot the trend line
-            axis_twin.plot(x, votes_trend(x), ":")
+            #axis_twin.plot(x, votes_trend(x), ":")
 
         for axis in axes:
             axis.get_xaxis().set_visible(False)
